5e4215943600d7e38ef76675bd7a26671402231d
bezumkin <bezumkin@yandex.ru>
2015-02-06 02:00:32 +0700

    Improved manager log processor

--- core/components/mxmanager/processors/main/log/getlist.class.php
+++ core/components/mxmanager/processors/main/log/getlist.class.php
@@ -12,8 +12,6 @@ class mxSystemLogGetListProcessor extends modSystemLogGetListProcessor {
 			$logArray['item'],
 			$logArray['classKey']
 		);
-		$logArray['occurred'] = str_replace(', ', "\n", $logArray['occurred']);
-		$logArray['action'] = str_replace('_', "\n", $logArray['action']);
 		$logArray['name'] = html_entity_decode($logArray['name'], ENT_QUOTES, $this->modx->getOption('modx_charset', null, 'UTF-8'));
 
 		return $logArray;
