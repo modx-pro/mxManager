d69a89380de03eb8dda6fe8173ba0ec5681a0d17
bezumkin <bezumkin@yandex.ru>
2015-05-10 20:26:49 +0700

    Version 1.0.0-pl

--- _build/build.config.php
+++ _build/build.config.php
@@ -5,7 +5,7 @@ define('PKG_NAME', 'mxManager');
 define('PKG_NAME_LOWER', strtolower(PKG_NAME));
 
 define('PKG_VERSION', '1.0.0');
-define('PKG_RELEASE', 'beta');
+define('PKG_RELEASE', 'pl');
 define('PKG_AUTO_INSTALL', true);
 define('PKG_NAMESPACE_PATH', '{core_path}components/' . PKG_NAME_LOWER . '/');
 
--- core/components/mxmanager/model/mxmanager/mxmanager.class.php
+++ core/components/mxmanager/model/mxmanager/mxmanager.class.php
@@ -6,7 +6,7 @@
 class mxManager {
 	/* @var modX $modx */
 	public $modx;
-	protected $_version = "1.0-beta";
+	protected $_version = "1.0.0-pl";
 
 
 	/**
