a2d5a172e2b28a1c171445a1c6e1498fb7fe574b
bezumkin <bezumkin@yandex.ru>
2015-08-21 01:37:45 +0700

    - Removed unnecessary files. Version 1.0.2-pl

--- _build/build.transport.php
+++ _build/build.transport.php
@@ -273,10 +273,7 @@ $builder->setPackageAttributes(array(
 	'changelog' => file_get_contents($sources['docs'] . 'changelog.txt'),
 	'license' => file_get_contents($sources['docs'] . 'license.txt'),
 	'readme' => file_get_contents($sources['docs'] . 'readme.txt'),
-	'chunks' => $BUILD_CHUNKS,
-	'setup-options' => array(
-		'source' => $sources['build'] . 'setup.options.php',
-	),
+	//'chunks' => $BUILD_CHUNKS,
 ));
 $modx->log(modX::LOG_LEVEL_INFO, 'Added package attributes and setup options.');
 
--- core/components/mxmanager/elements/templates/home.tpl
+++
@@ -1,1 +0,0 @@
-<div id="mxmanager-panel-home-div"></div>
